8340fc2fb4ded3a674799bd454fbe8a58c7a25c5
Fullmetalre <109579847+Fullmetalre@users.noreply.github.com>
2025-11-24 01:47:08 +1000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+import java.sql.SQLOutput;
+
+public class Main {}
+public static void main(String[] args) {
+    System.out.println("Задача 1 ");
+double dog = 0.8 ;
+double cat = 3.6;
+int paper = 763789;
+System.out.println(dog);
+System.out.println(cat);
+System.out.println(paper);
+
+
+System.out.println("Задача 2 ");
+ (dog) = 0.8 + 4;
+    (cat) = 3.6 + 4;
+    (paper) = 763789 + 4;
+    System.out.println(dog);
+    System.out.println(cat);
+    System.out.println(paper);
+
+
+    System.out.println("Задача 3 ");
+    (dog) = 4.8 - 3.5;
+    (cat) = 7.6 - 3.5;
+    (paper) = 763793 - 3;
+    System.out.println(dog);
+    System.out.println(cat);
+    System.out.println(paper);
+
+
+    System.out.println("Задача 5 ");
+    var frog = 3.5;
+    System.out.println(frog);
+    (frog) = frog * 10 / 3.5 + 4;
+    System.out.println(frog);
+
+
+    System.out.println("Задача 4 ");
+    var friend = 19;
+    System.out.println(friend);
+    friend = (friend = 19 + 2);
+    System.out.println(friend);
+    friend = (friend = 21 / 7);
+    System.out.println(friend);
+
+
+    System.out.println("Задача 6 ");
+    var boxer1Weight = 78.2;
+    var boxer2Weight = 82.7;
+    System.out.println(boxer1Weight);
+    System.out.println(boxer2Weight);
+var weight1= 78.2;
+var weight2 = 82.7;
+System.out.println(weight1 + " kg " );
+System.out.println(weight2 + " kg " );
+    var difference = boxer1Weight - boxer2Weight;
+    System.out.println(difference);
+
+System.out.println("Задача 7 ");
+    var totalNumberOfHours = 640;
+    var timeOfEachEmployee = 8;
+    var totalEmployees = totalNumberOfHours / timeOfEachEmployee;
+    System.out.println("Всего работников в компании - " + totalEmployees + "  человек");
+
+
+    System.out.println("Задание 8 (2)");
+
+    var increaseBy94TotalEmployees =  totalEmployees  + 94;
+    var totalTimeIfIncreaseBy94Employees =  increaseBy94TotalEmployees * timeOfEachEmployee;
+    System.out.println(increaseBy94TotalEmployees);
+    System.out.println("Если в компании работает - " + increaseBy94TotalEmployees + "  человека," +
+            " то всего " + totalTimeIfIncreaseBy94Employees +  " часов работы может быть поделено между сотрудниками"
+    );
+
+}
